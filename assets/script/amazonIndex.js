203a7b245e82cf323376c0b202babcbce37346f7
JodieTon <jodie.tonna.h63221@mcast.edu.mt>
2026-05-01 20:13:20 +0200

    Continuation of page
    Fixed the top nav
    Added 2 slidable sections
    Added 2 box options similar to Amazon's

--- assets/script/amazonIndex.js
+++ assets/script/amazonIndex.js
@@ -3,3 +3,41 @@ document.querySelectorAll('.navbar .nav-link').forEach(link => {
 
     });
 });
+
+const sliders = document.querySelectorAll('.deals-cards');
+
+sliders.forEach(slider => {
+    let isDown = false;
+    let startX;
+    let scrollLeft;
+
+    slider.addEventListener('mousedown', (e) => {
+        isDown = true;
+
+        startX = e.pageX - slider.offsetLeft;
+        scrollLeft = slider.scrollLeft;
+
+        document.body.style.overflowX = 'hidden';
+    });
+
+    slider.addEventListener('mouseup', () => {
+        isDown = false;
+        document.body.style.overflowX = '';
+    });
+
+    slider.addEventListener('mouseleave', () => {
+        isDown = false;
+        document.body.style.overflowX = '';
+    });
+
+    slider.addEventListener('mousemove', (e) => {
+        if (!isDown) return;
+
+        e.preventDefault();
+
+        const x = e.pageX - slider.offsetLeft;
+        const walk = (x - startX) * 2;
+
+        slider.scrollLeft = scrollLeft - walk;
+    });
+});
